f13efa6095aa635ec7372b0d3bbf02071578a8e2
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2025-07-25 08:21:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import json
-import requests
 
-r = requests.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/USD/")
-
-data = json.loads(r.content)
-kurs = float(data[0]['Rate'])
-
-amount = float(input("So'm kiriting: "))
-print(round(amount / kurs, 2))
